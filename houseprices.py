2ed51a345cadd77b0d655bc0142870ca6cf6902f
Andy Kilner <gnublade@gmail.com>
2013-11-17 13:40:17 +0000

    Scrape multiple pages

--- houseprices.py
+++ houseprices.py
@@ -8,13 +8,39 @@ URL = "http://www.primelocation.com/house-prices/london/tollington-road/?q=Tolli
 def get_house_prices():
     response = requests.get(URL)
     soup = BeautifulSoup(response.content)
+    entries = scrape_entries(soup)
+    entries = filter_entries(entries)
+    output_entries(entries)
+
+    for next_page_url in get_pages(soup):
+        entries = scrape_entries(soup)
+        entries = filter_entries(entries)
+        output_entries(entries)
+
+
+def get_pages(soup):
+    page_urls = set(e['href'] for e in soup.select('.paginate a'))
+    return page_urls
+
+
+def scrape_entries(soup):
     for row in soup.select('.searchresults tbody tr'):
         date_col, address_col, price_col = row.find_all('td')
         date = date_col.find('strong').get_text().strip()
         price = price_col.get_text().strip()
         address = address_col.find('h2').get_text().strip()
+        yield (date, address, price)
+
+
+def filter_entries(entries):
+    for date, address, price in entries:
         if 'Apartment' in address:
-            print ", ".join([date, address, price])
+            yield (date, address, price)
+
+
+def output_entries(entries):
+    for date, address, price in entries:
+        print ", ".join([date, address, price])
 
 
 if __name__ == '__main__':
